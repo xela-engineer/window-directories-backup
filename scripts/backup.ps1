83363cd5d56c16f76bb968fbebefa44eac03e618
Alex Woo <alex23woo@gmail.com>
2024-02-12 22:12:59 +0800

    Update backup.ps1

--- scripts/backup.ps1
+++ scripts/backup.ps1
@@ -20,9 +20,16 @@ write-output $backupStoragePath
 # Test-Script -Name Mother -Age 29
 
 Write-Output "Start Mirroring Working Directory"
+# TODO: confirm can we access to the source directory or not
+
+# TODO: confirm do we have the destination directory or not. If not, create one.
+
+# TODO: confirm do we have the log directory. If do not exist, create one.
 
 Robocopy $workingDirPath $backupStoragePath /MIR /R:3 /W:60 /E /ZB /MT:16 /LOG+:$copyLogFilePath
 
+# TODO: what is the exit code of the above command?
+# TODO: If not good, sent alert.
 
 $url = 'https://slack.com/api/chat.postMessage'
 $message = 'Working Directory Backup Success'
@@ -30,3 +37,5 @@ $token = $slackToken
 $channel = 'working-space'
 $body = @{token = $token; channel = $channel; text = $message; pretty = 1 }
 Invoke-WebRequest -Uri $url -Method POST -Body $body
+
+# TODO: maintain the achieve versioning
